00ea1a78f4c1f6a236d3f52a90abfacb29dd1ead
Purbayan Chowdhury <pur.cho.99@gmail.com>
2025-04-25 11:34:08 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,23 +1,32 @@
 import socket  # noqa: F401
 import os
+import threading
 from argparse import ArgumentParser
 from .pub_server import handle_request
 
+
+def server_run(client_socket: socket.socket):
+    while True:
+        req_buff = client_socket.recv(1024)
+        if req_buff is None:
+            break
+        client_socket.sendall(handle_request(req_buff))
+
+        break
+
+
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    client_socket, _ = server_socket.accept() # wait for client
+    print(f"Server started on {server_socket.getsockname()}")
+    client_socket, addr = server_socket.accept()  # wait for client
 
     try:
-        while True:
-            req_buff = client_socket.recv(1024)
-            if req_buff is None:
-                break
-            client_socket.sendall(handle_request(req_buff))
-
-            break
+        print(f"Connected to {addr}")
+        threading.Thread(target=server_run, args=(client_socket,))
     finally:
         client_socket.close()
         server_socket.close()
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
